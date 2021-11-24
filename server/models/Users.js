d62f80b5ade11c93fae506bc6ef92b96f6098987
metepg <mete.guneysel@gmail.com>
2021-11-24 10:58:38 +0200

    Folder restructure

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    name:{type:String,required:true},
-    password:{type:String,required:true},
-    id:{type:String,required:true}
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  id: { type: String, required: true },
 });
-schema.set("toJSON",{
-    virtuals:true,
-    transform:(document, returnedObject) => {
-        returnedObject.id = returnedObject_id.toString();
-        delete returnedObject_id;
-        delete returnedObject.__v;
-    }
+schema.set('toJSON', {
+  virtuals: true,
+  transform: (document, returnedObject) => {
+    // returnedObject.id = returnedObject_id.toString();
+    // delete returnedObject_id;
+    // delete returnedObject.__v;
+  },
 });
 
-module.exports = mongoose.model("users", schema)
-
+module.exports = mongoose.model('users', schema);
