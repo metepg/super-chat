065344acee8dce05de7a341dcac7249d16a85b19
metepg <mete.guneysel@gmail.com>
2021-11-25 09:56:12 +0200

    Lisäsin corsin (selain saa yhteyden palvelimeen) ja endpointin mitä front end käyttää kun lähettää viestejä

--- server/index.js
+++ server/index.js
@@ -1,16 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL).catch((error) => console.log(error));
-console.log(mongoose.connection.readyState);
+// Importit
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-app.use(express.json());
 
-app.get('/ping', (req, res) =>
+// Middlewaret
+app.use(express.json());
+app.use(cors());
+
+// Koodi
+const port = process.env.PORT;
+mongoose.connect(process.env.MONGO_URL).catch((error) => console.log(error));
+
+mongoose.connection.once("open", () => {
+  console.log("Succesfully connected to MONGODB");
+});
+
+mongoose.connection.once("error", (e) => {
+  console.log(`MongoDB error ${e}`);
+});
+
+app.get("/ping", (req, res) =>
   res.send("<script>console.log('yeah')</script>")
 );
-app.post('/post', (req, res) => console.log(req.body));
-const port = process.env.PORT;
+app.post("/post", (req, res) => console.log(req.body));
+
+// Malli endpointista POST /api/messages
+// req oliosta saa vastaanotettavaa tietoa esim (req.body) antaa lähetetyn jsonin oliona
+// res oliolla voi lähettää pingaajalle vastauksen json muodossa esim res.json({message: 'hyvin menee'})
+// voi myös lähettää status codeja (suositeltavaa) kuten 404 tai 200 esim res.status(400).json({ error: 'message' })
+// status codet on hyödyllisiä debugatessa
+app.post("/api/messages", (req, res) => console.log(req.body));
+
 app.listen(port, () => console.log(`SUPER CHAT RUNNING ON PORT ${port}`));
