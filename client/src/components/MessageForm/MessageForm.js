6cefa56d50c565f5515d5a016cdfdc999eea843b
metepg <mete.guneysel@gmail.com>
2021-11-25 10:05:24 +0200

    Lisätty axios kirjasto joka helpottaa http pyyntöjen tekemistä. Lisäsin komponentin MessageForm joka lähettää backendiin viestin(tätä voi muokata tai käyttää jos haluaa

---
+++ client/src/components/MessageForm/MessageForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { createMessage } from '../../api/messages';
+
+const MessageForm = () => {
+  // message = inputin sen hetkinen arvo
+  // setMessage = asettaa inputille arvon kun kenttään kirjoitetaan
+  const [message, setMessage] = useState('');
+  function validate(e) {
+    // e.preventDefault estää sivun päivittymisen (default toiminto 90 luvulta kun lomakkeilla lähetettiin dataa)
+    e.preventDefault();
+
+    // Jos viestikenttä on tyhjä älä tee mitään
+    if (!message) {
+      return;
+    }
+    createMessage({ message });
+  }
+
+  return (
+    <form onSubmit={(e) => validate(e)}>
+      <input
+        placeholder="Type something"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">SEND</button>
+    </form>
+  );
+};
+
+export default MessageForm;
