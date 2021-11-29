284fa095a921df3f3664f72bbef2117c0f76b4fc
mkws238 <73102280+mkws238@users.noreply.github.com>
2021-11-29 23:01:31 +0200

    messagefeed ja chatbox paivitys

--- client/src/components/ChatBox/Chatbox.js
+++ client/src/components/ChatBox/Chatbox.js
@@ -3,12 +3,49 @@ import css from './ChatBox.module.css';
 
 const ChatBox = () => {
   return (
-    <section className={css.controller}>
-      <aside className={css.aside}></aside>
-      <div className={css.feed}>
-        <MessageFeed />
-      </div>
-    </section>
+      <section className={css.controller}>
+        <div className={css.chatMessages} data-chat-id={'416814'}>
+          <div className={css.message} id={'msg-1'}>
+            <div className="msg msg-from-1756884" title="11/25/2021 - 9:15:10 AM">
+              <span className={css.name}>user_1</span>
+              <span className={css.teksti}>This is my message</span>
+            </div>
+          </div>
+        </div>
+        <div className={css.toppaneleft}>
+          <div className={css.inputsUsernameSignUp}>
+            <input
+                placeholder="Username"
+                className={css.inputUsername}
+                type="text"
+                name="username"
+            />
+            <button className={css.signUpButton}>Sign Up</button>
+          </div>
+
+          <div className={css.inputsLoginPassword}>
+            <input
+                placeholder="Password"
+                className={css.inputPassword}
+                type="text"
+                name="password"
+            />
+            <button className={css.loginButton}>Login</button>
+          </div>
+        </div>
+        <div className={css.toppaneright}>
+          <h1 className={css.usersText}>users</h1>
+        </div>
+
+        <aside className={css.aside}>
+          <ul className={css.usersList}>
+            <li>user_1</li>
+          </ul>
+        </aside>
+        <div className={css.feed}>
+          <MessageFeed />
+        </div>
+      </section>
   );
 };
 export default ChatBox;
--- client/src/components/MessageFeed/MessageFeed.js
+++ client/src/components/MessageFeed/MessageFeed.js
@@ -1,7 +1,18 @@
 import css from './MessageFeed.module.css';
+import React from 'react';
 
 const MessageFeed = () => {
-  return <div className={css.box}>feed</div>;
+  return (
+      <div className={css.box}>
+        <input
+            placeholder="Write something..."
+            className={css.inputChat}
+            type="text"
+            name="fname"
+        />
+        <button className={css.sendButton}>Send</button>
+      </div>
+  );
 };
 
 export default MessageFeed;
