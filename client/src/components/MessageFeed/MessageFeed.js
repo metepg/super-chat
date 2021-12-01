02e65a6f0a27aeba73f837192ba108b4053b7db0
mkws238 <73102280+mkws238@users.noreply.github.com>
2021-12-01 16:11:17 +0200

    poistettu import MessageForm2

--- client/src/components/MessageFeed/MessageFeed.js
+++ client/src/components/MessageFeed/MessageFeed.js
@@ -1,13 +1,11 @@
 import css from './MessageFeed.module.css';
 import React from 'react';
-import MessageForm2 from '../MessageForm/MessageForm2';
 import MessageForm from '../MessageForm/MessageForm';
 
 const MessageFeed = () => {
   return (
       <div className={css.box}>
         <MessageForm />
-
       </div>
   );
 };
